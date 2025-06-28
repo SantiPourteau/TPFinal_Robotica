596f2d8cc8c986ac09101259c9a6b50184f02935
Santiago Pourteau <spourteau@udesa.edu.ar>
2025-06-28 15:11:41 -0300

    TP

--- build/turtlebot3_slam_mapper/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/Users/Colegio/miniforge3/envs/rosenv':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/Users/Colegio/Documents/4to/1er_Semestre/Robotica/TPFinal_Robotica/install/turtlebot3_slam_mapper'
--- build/turtlebot3_slam_mapper/share/turtlebot3_slam_mapper/hook/pythonpath_develop.ps1
+++
@@ -1,3 +0,0 @@
-# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
-
-colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/Users/Colegio/Documents/4to/1er_Semestre/Robotica/TPFinal_Robotica/build/turtlebot3_slam_mapper"
